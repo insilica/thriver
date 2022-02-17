bbfb5b4dd1b4c10f61da6b768f7f2337a18816da
James Borden <jmborden@gmail.com>
2022-02-17 18:34:41 -0500

    better get_file function

--- R/API.R
+++ R/API.R
@@ -1,12 +1,10 @@
 library(httr)
-library(dplyr)
 
 #' Get all of the patient ids for a practice
 #'
 #' @param practice_id The practice_id to obtain the list
-#' @returns graphql_query S3 object
+#' @returns A list of patients ids
 #' @examples get_patient_ids(1)
-#' @export
 get_patient_ids <- function(practice_id) {
   query <- sprintf("{Practice(id:%s){patients{id}}}",
                       practice_id)
@@ -16,9 +14,9 @@ get_patient_ids <- function(practice_id) {
 #' Get all of the files associated with a patient
 #'
 #' @param patient_id the id of the patient to obtain the list for
-#' @returns graphql_query S3 object
+#' @returns A list of objects containing the filename, url and uuid of each
+#'          file associated with a patient
 #' @examples get_patient_file_list(4)
-#' @export
 get_patient_file_list <- function(patient_id) {
   query <- sprintf("{Patient(id:%s){files{filename,url,uuid}}}",patient_id)
   graphql_query(query)
@@ -30,26 +28,13 @@ get_patient_file_list <- function(patient_id) {
 #'
 #' @param first_name First Name of patient
 #' @param last_name Last Name of patient
-#' @param practice_id The id of the practice to create this patient with
+#' @practice_id The id of the practice to create this patient with
 #'
-#' @returns graphql_query S3 object
+#' @returns An object with new patient id
 #' @examples create_patient("Foo","Bar",1)
-#' @export
 create_patient <- function(first_name,last_name,practice_id) {
   query <- sprintf("mutation createPatient{createPatient(patient:{firstName:\"%s\",lastName:\"%s\",practiceId:%i}){id}}",
                      first_name,last_name,practice_id)
-  graphql_query(query)
-}
-
-#' Get all of the patient reports for a practice.
-#'
-#' @param practice_id The practice id to obtain reports for
-#' @return graphql_query S3 object
-#' @examples get_patient_reports(1)
-#' @export
-get_patient_reports <- function(project_id) {
-  query <- sprintf("{PracticePatientReports(practiceId:%s) {patientReports {patient {mrn firstName lastName}
-dropoutRisk relapseRisk}}}",project_id)
   graphql_query(query)
 }
 
--- R/client.R
+++ R/client.R
@@ -1,7 +1,7 @@
 library(httr)
-library(dplyr)
 library(keyring)
 library(jsonlite)
+library(utils)
 base_url = "https://thriveapp.health/api"
 
 #' Set your thrive API key.
@@ -40,13 +40,13 @@ graphql_query <- function(query) {
                     httr::add_headers('Authorization'=paste0("Bearer ",get_thrive_api_key())))
 
   parsed <- jsonlite::fromJSON(content(resp, "text",encoding="UTF-8"), simplifyVector = FALSE)
-  
+
   if(resp$status_code != 200) {
     stop(content(resp))
   } else if(!is.null(parsed$errors)){
     stop(parsed)
   }
-  
+
   structure(
     list(
       content = parsed,
@@ -67,18 +67,19 @@ print.graphql_query <- function(x, ...) {
 #' Get a patient file from the server and
 #' save it locally in getwd().
 #'
-#' @param url A url representing the location of the file on the Thrive server
+#' @param url A uri representing the location of the file on the Thrive server
 #'            Use `get_patient_file_list` to get the proper url and filename for
 #'            files associated with a patient
 #' @param filename filename to save data to locally
 #' @return A string representing the location of the file locally
-#' @examples get_patient_file("/files/patient/599/d4aa531a-89a0-4720-9060-bb32fa6cecec","report.pdf")
+#' @examples get_file("http://localhost:3001/files/patient/628/ed2b036a-63ea-4d0a-84b9-ea1cc7109db0/image%20(1).png",
+#'                             "image (1).png")
 #' @export
 get_file <- function(url,filename) {
   api_key_check()
-  httr::GET(paste0(base_url,url),
-            httr::add_headers('Authorization' = paste0("Bearer ", get_thrive_api_key()))) %>%
-    content("raw") %>%
+  httr::GET(URLencode(url),
+            httr::add_headers('Authorization' = paste0("Bearer ", get_thrive_api_key()))) |<
+    content("raw") |>
     writeBin(filename)
 
   return(paste0(getwd(),"/",filename))
@@ -98,7 +99,7 @@ post_file <- function(patient_id,filename) {
                     httr::add_headers('Authorization' = paste0("Bearer ", get_thrive_api_key())),
                     encode="multipart")
   parsed <- jsonlite::fromJSON(content(resp, "text",encoding="UTF-8"), simplifyVector = FALSE)
-  
+
   if(resp$status_code != 200) {
     stop(content(resp,"text"))
   } else {
@@ -120,7 +121,7 @@ delete_file <- function(patient_id,uuid) {
                httr::add_headers('Authorization' = paste0("Bearer ", get_thrive_api_key())))
 
   parsed <- jsonlite::fromJSON(content(resp, "text",encoding="UTF-8"), simplifyVector = FALSE)
-  
+
   if(resp$status_code != 200) {
     stop(content(resp,"text"))
   } else {
